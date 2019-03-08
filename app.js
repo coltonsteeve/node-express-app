8f3453374a3910d761a462f19b640e7d7bd00f9c
coltonsteeve <coltonsteeve@gmail.com>
2019-03-08 00:45:58 -0600

    Added public folder with index.html, main.css, and main.js, as well as getting app.js to use the public folder for the node server

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1'   // set constants
-const port = 3002
+const hostname = '0.0.0.0';   // set constants
+const port = 3002;
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Welcome home!')
+  res.send('Welcome home!');
 })
 
 app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.get('/big',  (req, res) =>{
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>');
 })
 
 app.get('/greeting/:id',  (req, res) =>{
-  res.send('Hello! The id was ' + req.params.id)
+  res.send('Hello! The id was ' + req.params.id);
 })
 
 app.get('/yo/:buddy',  (req, res) =>{
-  res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
+  res.send('<h1>Yo, ' + req.params.buddy + '!</h1>');
+})
+
+app.get('/map/:location', (req, res) =>{
+  res.send('<h1>' + req.params.location + ' is a cool place!</h1>');
+})
+
+app.get('/people', (req, res) =>{
+  const peopleArray = ['Bob', 'Sarah', 'Jim'];
+  res.send(peopleArray[0] + ', ' + peopleArray[1] + ', and ' + peopleArray[2] + ' are all interesting people!');
 })
 
 // handle non-existant routes
@@ -30,7 +41,7 @@ app.use((req, res, next) => {
 })
 
 app.listen(port, hostname, () => {
-  console.log(`Example app listening at http://${hostname}:${port}/`)
-  console.log('Hit CTRL-C CTRL-C to stop\n')
+  console.log(`Example app listening at http://${hostname}:${port}/`);
+  console.log('Hit CTRL-C CTRL-C to stop\n');
 })
 
